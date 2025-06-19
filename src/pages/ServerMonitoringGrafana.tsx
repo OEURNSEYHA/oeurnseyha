bfbe96bee603e796eec1390c8bfdcb16556fee59
seyha.oeurn <seyhaoeurn920@gmail.com>
2025-06-19 23:23:14 +0700

    update

--- src/pages/ServerMonitoringGrafana.tsx
+++ src/pages/ServerMonitoringGrafana.tsx
@@ -7,7 +7,7 @@ import workflow from "../Image/gafrana.drawio.svg";
 const ServerMonitoringGrafana: React.FC = () => {
   return (
     <div className="max-w-5xl mx-auto px-4 py-10">
-      <h1 className="text-4xl font-bold mb-6 text-center">
+      <h1 className="text-4xl font-bold mb-6 text-center text-black">
        Monitor Server with Grafana, Prometheus & Node Exporter
       </h1>
 
